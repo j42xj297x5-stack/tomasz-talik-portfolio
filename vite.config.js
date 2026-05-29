2eac3d497b9a55c9e6a4b873329929091cd0f69b
tap chan <j42xj297x5@privaterelay.appleid.com>
2026-05-29 21:05:04 +0200

    Set base path for portfolio deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/tomasz-talik-portfolio/',
   resolve: {
     alias: {
       three: fileURLToPath(new URL('./vendor/three/three.module.js', import.meta.url)),
