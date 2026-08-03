23a2c5fcf9cef2549c6c2fa47ddf445b42d053b6
tap chan <j42xj297x5@privaterelay.appleid.com>
2026-08-03 22:50:04 +0200

    Fix Astro Furnace extraction panel telemetry

---
+++ src/xr/furnace/createVrAstroFurnaceProcessSource.js
@@ -0,0 +1,10 @@
+export function createVrAstroFurnaceProcessSource(getInteraction) {
+  const interaction = () => getInteraction?.();
+  return {
+    getState: () => interaction()?.getState?.() ?? 'IDLE',
+    getProgress: () => interaction()?.getProgress?.() ?? 0,
+    getExtractionProgress: () => interaction()?.getExtractionProgress?.() ?? 0,
+    getAngularSpeed: () => interaction()?.getAngularSpeed?.() ?? 0,
+    getProcessAngle: () => interaction()?.getProcessAngle?.() ?? 0
+  };
+}
