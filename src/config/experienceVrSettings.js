4c757fb238513127f3a38f826335be44ae8dbbab
tap chan <j42xj297x5@privaterelay.appleid.com>
2026-08-24 22:32:12 +0200

    fix(vr): align sun with key light

--- src/config/experienceVrSettings.js
+++ src/config/experienceVrSettings.js
@@ -24,9 +24,10 @@ export const DEFAULT_EXPERIENCE_VR_SETTINGS = Object.freeze({
     revealDurationSeconds: 60,
     sun: {
       distanceFromWorldCenter: 110,
+      scale: 3,
       emissiveColor: '#ffb85c',
       emissiveIntensity: 0.25,
-      light: { color: '#fff1d6', intensity: 8 },
+      light: { color: '#fff1d6', intensity: 40 },
       facingCorrectionDegrees: { x: 0, y: 0, z: 0 }
     },
     stars: {
