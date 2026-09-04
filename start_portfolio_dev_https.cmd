00aecec9c6cbc42ac6afd784abecad144be7c939
tap chan <j42xj297x5@privaterelay.appleid.com>
2026-09-04 13:01:45 +0200

    zmiana pliku cmd

--- start_portfolio_dev_https.cmd
+++ start_portfolio_dev_https.cmd
@@ -1,1 +1,1 @@
-cmd /k "cd /d C:\Users\Legio\Documents\tomasz-talik-portfolio\tomasz-talik-portfolio && echo KATALOG OK && echo. && node -v && npm.cmd -v && echo. && npm.cmd install && echo. && npm.cmd run dev"
+cmd /k "cd /d C:\Users\Legio\Documents\tomasz-talik-portfolio\tomasz-talik-portfolio && echo KATALOG OK && echo. && node -v && npm.cmd -v && echo. && echo. && npm.cmd run dev"
